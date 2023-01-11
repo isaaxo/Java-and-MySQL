798e2a50a01ddd621875ae6b24dae4db29942d84
Isaac Makhumisane <101597713+isaaxo@users.noreply.github.com>
2023-01-11 05:57:26 +0200

    Add files via upload

---
+++ Compulsory Tasks/src/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Architect"},{"p":"<Unnamed>","l":"Contractor"},{"p":"<Unnamed>","l":"Customer"},{"p":"<Unnamed>","l":"Menu"},{"p":"<Unnamed>","l":"Projects"}];updateSearchResults();
